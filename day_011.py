ed816208099e18337aad0e4d328913ff8ecc1fcb
GitZW <152408892@qq.com>
2020-05-23 08:37:59 +0700

    day_011

---
+++ day_011.py
@@ -0,0 +1,31 @@
+"""
+Implement an algorithm to find the kth to last element of a singly linked list. Return the value of the element.
+
+Note: This problem is slightly different from the original one in the book.
+
+Example:
+
+Input:  1->2->3->4->5 和 k = 2
+Output:  4
+Note:
+
+k is always valid.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def kthToLast(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: int
+        """
