658fd0987cad42f0fdb8b5745054aff312191e64
GitZW <474268013@qq.com>
2020-05-26 21:31:04 +0700

    day 014

---
+++ day_014.py
@@ -0,0 +1,26 @@
+"""
+Implement a function to check if a linked list is a palindrome.
+
+ 
+
+Example 1:
+
+Input:  1->2
+Output:  false
+Example 2:
+
+Input:  1->2->2->1
+Output:  true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-linked-list-lcci
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
