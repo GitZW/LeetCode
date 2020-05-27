74472fb71c95297b1fedf6f291aae4c2bed3fbd7
GitZW <474268013@qq.com>
2020-05-27 07:44:29 +0700

    day 014

--- day_014.py
+++ day_014.py
@@ -16,11 +16,26 @@ Output:  true
 链接：https://leetcode-cn.com/problems/palindrome-linked-list-lcci
 """
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
+
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
+        stack = []
+        copy = head
+        while head:
+            stack.append(head.val)
+            head = head.next
+
+        while copy:
+            if not copy.val == stack.pop():
+                return False
+
+            copy = copy.next
+
+        return True
