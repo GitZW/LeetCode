2e5898f995453d1165648ab9285d07b942a11bae
GitZW <474268013@qq.com>
2020-06-03 22:43:38 +0700

    day 024 优化

--- day_024.py
+++ day_024.py
@@ -63,3 +63,22 @@ class Solution(object):
             return False
 
         return True
+
+
+class Solution2:
+    def IsBalanced_Solution(self, pRoot):
+        if not pRoot:
+            return True
+        if self.DeepTree(pRoot) == -1:
+            return False
+        else:
+            return True
+
+    def DeepTree(self, pRoot):
+        if not pRoot:
+            return 0
+        left = self.DeepTree(pRoot.left)
+        right = self.DeepTree(pRoot.right)
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return 1 + max(left, right)
