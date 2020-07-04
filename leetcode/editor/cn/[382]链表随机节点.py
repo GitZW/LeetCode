c5bba16c87ce8d62e7cf5ddf16e17b8b18e26e63
GitZW <474268013@qq.com>
2020-07-05 01:15:40 +0700

    day 061 蓄水池算法 随机M 个数

--- leetcode/editor/cn/[382]链表随机节点.py
+++ leetcode/editor/cn/[382]链表随机节点.py
@@ -82,7 +82,20 @@ class Solution2(object):
 
         return ans
 
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(head)
-# param_1 = obj.getRandom()
-# leetcode submit region end(Prohibit modification and deletion)
+# reservoir = [];
+#
+# for (int i = 0; i < reservoir.length; i++)
+# {
+#     reservoir[i] = dataStream[i];
+# }
+#
+# for (int i = m; i < dataStream.length; i++)
+# {
+#     // 随机获得一个[0, i]内的随机整数
+#     int d = rand.nextInt(i + 1);
+#     // 如果随机整数落在[0, m-1]范围内，则替换蓄水池中的元素
+#     if (d < m)
+#     {
+#         reservoir[d] = dataStream[i];
+#     }
+# }
