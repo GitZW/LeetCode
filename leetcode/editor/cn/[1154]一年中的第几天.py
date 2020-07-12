3006a350fab8dc05c1b744afd29031c772f4c1f6
GitZW <474268013@qq.com>
2020-07-13 00:26:06 +0700

    day 069

---
+++ leetcode/editor/cn/[1154]一年中的第几天.py
@@ -0,0 +1,61 @@
+# 给你一个按 YYYY-MM-DD 格式表示日期的字符串 date，请你计算并返回该日期是当年的第几天。 
+# 
+#  通常情况下，我们认为 1 月 1 日是每年的第 1 天，1 月 2 日是每年的第 2 天，依此类推。每个月的天数与现行公元纪年法（格里高利历）一致。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：date = "2019-01-09"
+# 输出：9
+#  
+# 
+#  示例 2： 
+# 
+#  输入：date = "2019-02-10"
+# 输出：41
+#  
+# 
+#  示例 3： 
+# 
+#  输入：date = "2003-03-01"
+# 输出：60
+#  
+# 
+#  示例 4： 
+# 
+#  输入：date = "2004-03-01"
+# 输出：61 
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  date.length == 10 
+#  date[4] == date[7] == '-'，其他的 date[i] 都是数字。 
+#  date 表示的范围从 1900 年 1 月 1 日至 2019 年 12 月 31 日。 
+#  
+#  Related Topics 数学
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def dayOfYear(self, date):
+        """
+        :type date: str
+        :rtype: int
+        """
+        year, month, day = map(int, date.split("-"))
+        month_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        ans = sum(month_day[:month-1]) + day
+        if month > 2 and (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)):
+            ans += 1
+
+        return ans
+
+
+s = Solution()
+print(s.dayOfYear("2004-03-01"))
+
+# leetcode submit region end(Prohibit modification and deletion)
