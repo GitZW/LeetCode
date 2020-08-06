b135dce5fe48af276399864d2f042ed708ec331b
GitZW <474268013@qq.com>
2020-08-07 00:29:14 +0700

    day 097

--- leetcode/editor/cn/[1417]重新格式化字符串.py
+++ leetcode/editor/cn/[1417]重新格式化字符串.py
@@ -51,10 +51,27 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
-class Solution(object):
-    def reformat(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-# leetcode submit region end(Prohibit modification and deletion)
+class Solution:
+    def reformat(self, s: str) -> str:
+        numbers=[]
+        letters=[]
+        for i in s:
+            if i.isalpha():
+                letters.append(i)
+            else:
+                numbers.append(i)
+        n,l=len(numbers),len(letters)
+        if abs(n-l)>1:
+            return ''
+        res=[]
+        if n>=l:
+            while numbers:
+                res.append(numbers.pop())
+                if letters:
+                    res.append(letters.pop())
+        else:
+            while letters:
+                res.append(letters.pop())
+                if numbers:
+                    res.append(numbers.pop())
+        return "".join(res)
