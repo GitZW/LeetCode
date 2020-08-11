9e881ccc68d77d0894a12c1d13c668ba1cb24bae
GitZW <474268013@qq.com>
2020-08-12 05:37:17 +0700

    day 102

---
+++ leetcode/editor/cn/[628]三个数的最大乘积.py
@@ -0,0 +1,32 @@
+# 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。 
+# 
+#  示例 1: 
+# 
+#  
+# 输入: [1,2,3]
+# 输出: 6
+#  
+# 
+#  示例 2: 
+# 
+#  
+# 输入: [1,2,3,4]
+# 输出: 24
+#  
+# 
+#  注意: 
+# 
+#  
+#  给定的整型数组长度范围是[3,104]，数组中所有的元素范围是[-1000, 1000]。 
+#  输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。 
+#  
+#  Related Topics 数组 数学
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maximumProduct(self, nums):
+        nums.sort()
+        return max(nums[0]*nums[1]*nums[-1],nums[-3]*nums[-2]*nums[-1])
+        
+# leetcode submit region end(Prohibit modification and deletion)
