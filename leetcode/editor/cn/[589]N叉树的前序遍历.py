fa0dce4e73e03c82d767e2029555023188f27dc5
GitZW <474268013@qq.com>
2020-09-15 06:50:22 +0700

    day 135

---
+++ leetcode/editor/cn/[589]N叉树的前序遍历.py
@@ -0,0 +1,45 @@
+# 给定一个 N 叉树，返回其节点值的前序遍历。 
+# 
+#  例如，给定一个 3叉树 : 
+# 
+#  
+# 
+#  
+# 
+#  
+# 
+#  返回其前序遍历: [1,3,5,6,2,4]。 
+# 
+#  
+# 
+#  说明: 递归法很简单，你可以使用迭代法完成此题吗? Related Topics 树
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        stack = []
+        stack.append(root)
+        ans = []
+        while stack:
+            cur = stack.pop()
+            ans.append(cur.val)
+            for e in reversed(cur.children):
+                stack.append(e)
+        return ans
+# leetcode submit region end(Prohibit modification and deletion)
