7401bbf0686504d330fae093b29984094e871ebb
GitZW <474268013@qq.com>
2020-09-23 23:41:51 +0700

    day 145

--- leetcode/editor/cn/[894]所有可能的满二叉树.py
+++ leetcode/editor/cn/[894]所有可能的满二叉树.py
@@ -29,17 +29,34 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
+    memo = {1: [TreeNode(0)]}
+
     def allPossibleFBT(self, N):
         """
         :type N: int
         :rtype: List[TreeNode]
         """
-        
+        if N % 2 == 0:
+            return
+        if N not in Solution.memo:
+            ans = []
+            for x in range(N):
+                y = N - 1 - x
+                for left in self.allPossibleFBT(x):
+                    for right in self.allPossibleFBT(y):
+                        bns = TreeNode(0)
+                        bns.left = left
+                        bns.right = right
+                        ans.append(bns)
+            Solution.memo[N] = ans
+
+        return Solution.memo[N]
 # leetcode submit region end(Prohibit modification and deletion)
