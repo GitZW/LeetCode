2c8024f27c2e73449bdd5aad639f3ce4556acdf8
GitZW <474268013@qq.com>
2020-09-28 23:40:38 +0700

    day 150

--- leetcode/editor/cn/[107]二叉树的层次遍历 II.py
+++ leetcode/editor/cn/[107]二叉树的层次遍历 II.py
@@ -28,11 +28,29 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+import collections
 
-class Solution(object):
+
+class Solution:
     def levelOrderBottom(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+        levelOrder = list()
+        if not root:
+            return levelOrder
+
+        q = collections.deque([root])
+        while q:
+            level = list()
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            levelOrder.append(level)
+
+        return levelOrder[::-1]
+
+
 # leetcode submit region end(Prohibit modification and deletion)
