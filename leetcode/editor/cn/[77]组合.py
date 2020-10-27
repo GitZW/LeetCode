e4826f216a1cae8cdce7f0a6ca8fc5883c9eadab
GitZW <474268013@qq.com>
2020-10-27 22:42:38 +0700

    day 182

---
+++ leetcode/editor/cn/[77]组合.py
@@ -0,0 +1,39 @@
+# 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。 
+# 
+#  示例: 
+# 
+#  输入: n = 4, k = 2
+# 输出:
+# [
+#   [2,4],
+#   [3,4],
+#   [2,3],
+#   [1,2],
+#   [1,3],
+#   [1,4],
+# ] 
+#  Related Topics 回溯算法
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+import itertools
+
+
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return list(itertools.combinations(range(1,n+1),k))
+
+
+class Solution2(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        pass
