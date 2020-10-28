000fa3704ee2a6708deeb4fe06ba3362474eba00
GitZW <474268013@qq.com>
2020-10-28 20:30:44 +0700

    day 182

--- leetcode/editor/cn/[77]组合.py
+++ leetcode/editor/cn/[77]组合.py
@@ -36,4 +36,23 @@ class Solution2(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        pass
+        nums = [i for i in range(1, n + 1)]
+
+        res = []
+
+        def backtrace(nums_b, curr_res, index):
+            # print("curr_res:",curr_res)
+            if len(curr_res) == k:
+                res.append(curr_res[:])
+                return
+
+            for i in range(index, n):
+                curr_res.append(nums[i])
+                backtrace(nums_b[index:], curr_res, i + 1)
+                curr_res.pop()
+
+        if n == 0 or k == 0:
+            return res
+
+        backtrace(nums, [], 0)
+        return res
