205ff73f3fa63e2b9432e81145196e0e5bfb8349
GitZW <474268013@qq.com>
2020-11-18 01:04:40 +0700

    day 197

--- leetcode/editor/cn/[824]山羊拉丁文.py
+++ leetcode/editor/cn/[824]山羊拉丁文.py
@@ -48,5 +48,13 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
-        
-# leetcode submit region end(Prohibit modification and deletion)
+
+        def convert(word):
+            if word[0] not in 'aeiouAEIOU':
+                word = word[1:] + word[:1]
+            return word + 'ma'
+
+        return " ".join(convert(word) + 'a' * i
+                        for i, word in enumerate(S.split(), 1))
+
+ 
