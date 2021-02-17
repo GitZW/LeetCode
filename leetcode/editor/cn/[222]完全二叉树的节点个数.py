a212ea3e51ee104559254fa01f310cab281623e6
GitZW <474268013@qq.com>
2021-02-17 21:41:30 +0700

    完全二叉树 的根节点 root ，求出该树的节点个数(2)

--- leetcode/editor/cn/[222]完全二叉树的节点个数.py
+++ leetcode/editor/cn/[222]完全二叉树的节点个数.py
@@ -58,4 +58,36 @@ class Solution(object):
         if not root:
             return 0
         return self.countNodes(root.right) + self.countNodes(root.left) + 1
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+
+
+# 对于最大层数为 hh 的完全二叉树，节点个数一定在 [2^h,2^{h+1}-1]的范围内，可以在该范围内通过二分查找的方式得到完全二叉树的节点个数。
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root: return 0
+        level = 0
+        node = root.left
+        while node:  # 这里获得层数
+            level += 1
+            node = node.left
+        l = 1 << level  # 左界
+        r = (l << 1) - 1  # 右界
+
+        while l < r:
+            mid = int((r + l + 1) / 2)  # 中位
+            node = root
+            path = 1 << (level - 1)  # 取mid号数的后几位的模板
+            while node and path > 0:
+                if mid & path:
+                    node = node.right
+                else:
+                    node = node.left
+                path >>= 1  # 一层查完，查下一层
+            if node:
+                l = mid  # 存在left位置变化
+            else:
+                r = mid - 1  # 不存在right位置变化
+        return r
